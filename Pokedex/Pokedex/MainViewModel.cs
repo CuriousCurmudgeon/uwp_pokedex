7c0ddd10675a51ca924aeed9885d842799d723ae
Brian Meeker <brian.meeker@infernored.com>
2016-07-17 16:13:27 -0400

    Generated a placeholder MainViewModel to tie to MainPage.

---
+++ Pokedex/Pokedex/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex
+{
+    public class MainViewModel
+    {
+        public string Test
+        {
+            get { return "Test Text"; }
+        }
+    }
+}
