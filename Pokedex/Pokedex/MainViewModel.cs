6ce374d62e4256d8ff8c7adfaa4696215a09cf2c
Brian Meeker <brian.meeker@infernored.com>
2016-07-17 22:19:18 -0400

    Implemented INotifyPropertyChanged on MainViewModel.

--- Pokedex/Pokedex/MainViewModel.cs
+++ Pokedex/Pokedex/MainViewModel.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pokedex
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        // Setting an empty delegate is not strictly necessary, but it removes the need
+        // for null checks for a miniscule performance hit.
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public MainViewModel()
+        {
+            Test = "Test Text";
+        }
+
+        private string _test;
         public string Test
         {
-            get { return "Test Text"; }
+            get { return _test; }
+            set
+            {
+                _test = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Manually setup property change notification so that the UI will be notified when values change.
+        // This allows bound values to update in the UI.
+        // I would usually use a framework like MvvmLight or MvvmCross to handle this for me.
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
