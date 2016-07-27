e66ebacd543e086922adff21edf5bc668ff1c3c9
Brian Meeker <brian.meeker@infernored.com>
2016-07-27 16:12:33 -0400

    Attempting to correctly display pokemon details.

--- Pokedex/Pokedex/Models/PokemonSprites.cs
+++ Pokedex/Pokedex/Models/PokemonSprites.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,28 @@ namespace Pokedex.Models
 {
     public class PokemonSprites
     {
+        [JsonProperty("back_female")]
         public string BackFemale { get; set; }
+
+        [JsonProperty("back_shiny_female")]
         public string BackShinyFemale { get; set; }
+
+        [JsonProperty("back_default")]
         public string BackDefault { get; set; }
+
+        [JsonProperty("front_female")]
         public string FrontFemale { get; set; }
+
+        [JsonProperty("front_shiny_female")]
         public string FrontShinyFemale { get; set; }
+
+        [JsonProperty("back_shiny")]
         public string BackShiny { get; set; }
+
+        [JsonProperty("front_default")]
         public string FrontDefault { get; set; }
+
+        [JsonProperty("front_shiny")]
         public string FrontShiny { get; set; }
     }
 }
