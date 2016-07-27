c353c294b9014a41f22aae836a735108ca84d86c
Brian Meeker <brian.meeker@infernored.com>
2016-07-27 16:03:11 -0400

    Renamed MainPage to MainView.

--- Pokedex.Windows/MainView.xaml.cs
+++ Pokedex.Windows/MainView.xaml.cs
@@ -21,9 +21,9 @@ namespace Pokedex.Windows
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainView : Page
     {
-        public MainPage()
+        public MainView()
         {
             this.InitializeComponent();
             VM = new MainViewModel();
